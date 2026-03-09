e55d6d49e4e5be3dcbbee9c862f4ff2e5c981c5b
Prabhanshu Narnaware <prabhanshuop@gmail.com>
2026-03-09 23:36:28 +0530

    added index.ts file and updated scripts

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(3000);
+//# sourceMappingURL=index.js.map
